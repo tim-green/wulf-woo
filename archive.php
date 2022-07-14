1de3a21254013c9a583a1e8e920fd030ed7a3f83
Tim <github@timgreen.ws>
2022-07-14 17:24:10 +1000

    created archive page

---
+++ archive.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<main id="primary" class="site-main">
+	
+    <?php if (have_posts()) :?>
+
+        <header class="page-header">
+            <?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+        </header>
+
+        <?php while ( have_posts() ) :
+				the_post();
+
+            get_template_part( 'templates/content/', 'loop', get_post_type());
+
+            endwhile;
+
+            the_posts_navigation();
+
+        else:
+
+            get_template_part( 'templates/content/loop', 'none');
+
+        endif;
+        ?>
+
+</main>
+
+<?php
+get_footer();
